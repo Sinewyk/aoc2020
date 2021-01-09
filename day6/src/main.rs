04dcd4bd955ef7274b9b8e1b0ba57a789f45cd5c
Sinewyk <sinewyk@gmail.com>
2021-01-09 19:38:25 +0100

    part1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,24 @@
+use std::collections::HashMap;
+
 fn main() {
-	let input = include_str!("input.txt");
+	let input = include_str!("input.txt")
+		.split("\n\n")
+		.collect::<Vec<&str>>();
+
+	let sum: usize = input
+		.iter()
+		.map(|x| {
+			let mut h: HashMap<char, bool> = HashMap::new();
+
+			x.split('\n').for_each(|s| {
+				s.chars().for_each(|c| {
+					h.insert(c, true);
+				});
+			});
+
+			return h.len();
+		})
+		.sum();
+
+	println!("part1: {}", sum);
 }
