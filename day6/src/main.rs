44fb20b7f9103c10456b63715e731f754407c914
Sinewyk <sinewyk@gmail.com>
2021-01-09 23:23:23 +0100

    day6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,24 +1,44 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn main() {
 	let input = include_str!("input.txt")
 		.split("\n\n")
 		.collect::<Vec<&str>>();
 
-	let sum: usize = input
-		.iter()
-		.map(|x| {
-			let mut h: HashMap<char, bool> = HashMap::new();
+	println!("part1: {}", part1(&input));
+	println!("part2: {}", part2(&input));
+}
 
-			x.split('\n').for_each(|s| {
-				s.chars().for_each(|c| {
-					h.insert(c, true);
+fn part1(split: &Vec<&str>) -> usize {
+	return split
+		.iter()
+		.map(|group| {
+			let mut h: HashSet<u8> = HashSet::new();
+
+			group.lines().for_each(|line| {
+				line.as_bytes().iter().for_each(|c| {
+					h.insert(*c);
 				});
 			});
 
 			return h.len();
 		})
 		.sum();
+}
 
-	println!("part1: {}", sum);
+fn part2(split: &Vec<&str>) -> usize {
+	let init: HashSet<u8> = (b'a'..=b'z').collect();
+
+	return split
+		.iter()
+		.map(|group| {
+			group
+				.split('\n')
+				.map(|line| line.as_bytes().iter().copied().collect::<HashSet<u8>>())
+				.fold(init.clone(), |acc, x| {
+					acc.intersection(&x).copied().collect()
+				})
+				.len()
+		})
+		.sum();
 }
