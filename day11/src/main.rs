4cbbc16c9ee07de54a4e6ca3f3fee7f44680df80
sinewyk <sinewyk@gmail.com>
2021-02-02 18:01:21 +0100

    part2, with some tests

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,6 +1,6 @@
-use std::convert::TryInto;
-
 use grid::Grid;
+use itertools::Itertools;
+use std::convert::TryInto;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 enum Cell {
@@ -30,6 +30,16 @@ fn main() {
 
 	println!(
 		"part1: {}",
+		final_grid
+			.iter()
+			.filter(|o| matches!(o, Cell::Occupied))
+			.count()
+	);
+
+	let final_grid = process(predict_part2, &initial_grid);
+
+	println!(
+		"part2: {}",
 		final_grid
 			.iter()
 			.filter(|o| matches!(o, Cell::Occupied))
@@ -82,6 +92,30 @@ fn predict_part1(grid: &Grid<Cell>) -> (Grid<Cell>, bool) {
 	(next_grid, b)
 }
 
+fn predict_part2(grid: &Grid<Cell>) -> (Grid<Cell>, bool) {
+	let mut next_grid = grid.clone();
+
+	grid_enumerate(grid).for_each(|(pos, c)| match *c {
+		Cell::Seat => {
+			let visible_seats = grid_first_visible_seats(grid, pos);
+			if visible_seats == 0 {
+				next_grid[pos.0][pos.1] = Cell::Occupied;
+			}
+		}
+		Cell::Occupied => {
+			let visible_seats = grid_first_visible_seats(grid, pos);
+			if visible_seats >= 5 {
+				next_grid[pos.0][pos.1] = Cell::Seat;
+			}
+		}
+		Cell::Floor => (),
+	});
+
+	let b = &next_grid == grid;
+
+	(next_grid, b)
+}
+
 fn grid_adjacent_occupied_seats(grid: &Grid<Cell>, pos: (usize, usize)) -> usize {
 	(-1isize..=1)
 		.map(|dx| (-1isize..=1).map(move |dy| (dx, dy)))
@@ -101,6 +135,34 @@ fn grid_adjacent_occupied_seats(grid: &Grid<Cell>, pos: (usize, usize)) -> usize
 				return false;
 			}
 		})
+		.count()
+}
+
+fn grid_first_visible_seats(grid: &Grid<Cell>, pos: (usize, usize)) -> usize {
+	(-1isize..=1)
+		.map(|dx| (-1isize..=1).map(move |dy| (dx, dy)))
+		.flatten()
+		.filter(|&(dx, dy)| !(dx == 0 && dy == 0))
+		.map(move |(dx, dy)| {
+			itertools::iterate((pos.0 as isize, pos.1 as isize), move |new_pos| {
+				(new_pos.0 + dx, new_pos.1 + dy)
+			})
+			.skip(1)
+			.map(move |pos| {
+				return grid.get(
+					pos.0.try_into().unwrap_or(usize::MAX),
+					pos.1.try_into().unwrap_or(usize::MAX),
+				);
+			})
+			.while_some()
+			.filter_map(|c| match c {
+				Cell::Floor => None,
+				seat => Some(seat),
+			})
+			.take(1)
+		})
+		.flatten()
+		.filter(|c| matches!(c, Cell::Occupied))
 		.count()
 }
 
@@ -192,11 +254,31 @@ fn test_adjacent() {
 		.collect::<Vec<&str>>(),
 	);
 
-	dbg!(grid_adjacent_occupied_seats(&grid, (0, 1)));
-
 	assert_eq!(grid_adjacent_occupied_seats(&grid, (0, 0)), 1);
 	assert_eq!(grid_adjacent_occupied_seats(&grid, (0, 1)), 2);
 	assert_eq!(grid_adjacent_occupied_seats(&grid, (1, 2)), 3);
+}
+
+#[test]
+fn test_visible() {
+	let grid = parse(
+		indoc::indoc!(
+			"
+			L.#LL
+			L#L.#
+			L##.#
+			.....
+			..L..
+			"
+		)
+		.lines()
+		.collect::<Vec<&str>>(),
+	);
+
+	assert_eq!(grid_first_visible_seats(&grid, (0, 0)), 2);
+	assert_eq!(grid_first_visible_seats(&grid, (0, 1)), 2);
+	assert_eq!(grid_first_visible_seats(&grid, (1, 2)), 5);
+	assert_eq!(grid_first_visible_seats(&grid, (4, 2)), 2);
 }
 
 fn grid_enumerate<'a, T>(grid: &'a Grid<T>) -> impl Iterator<Item = ((usize, usize), &T)> + 'a
